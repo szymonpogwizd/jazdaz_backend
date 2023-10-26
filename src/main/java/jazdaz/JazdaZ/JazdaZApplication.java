21dd2c71ea25def9ab1d5165c29876e07ee43dc1
szymonpogwizd <szymonpogwizd12@gmail.com>
2023-10-26 18:44:06 +0200

    Init commit

---
+++ src/main/java/jazdaz/JazdaZ/JazdaZApplication.java
@@ -0,0 +1,13 @@
+package jazdaz.JazdaZ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazdaZApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazdaZApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jazdaz/JazdaZ/JazdaZApplicationTests.java
@@ -0,0 +1,13 @@
+package jazdaz.JazdaZ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazdaZApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
