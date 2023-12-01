45ca190632690c5e0e5dc02c955f9e9e96008fa2
Szymon Pogwizd <116025180+szymonpogwizd@users.noreply.github.com>
2023-12-01 18:12:00 +0100

    JZ-49 - JWT Token configuration (#7)

---
+++ src/main/java/jazdaz/JazdaZ/utils/jwt/JwtResponse.java
@@ -0,0 +1,15 @@
+package jazdaz.JazdaZ.utils.jwt;
+
+import lombok.Getter;
+
+import java.io.Serializable;
+
+public class JwtResponse implements Serializable {
+
+    @Getter
+    private final String jwttoken;
+
+    public JwtResponse(String jwttoken) {
+        this.jwttoken = jwttoken;
+    }
+}
