d75c977b104c26ed1d485d9097ef382bd203b67a
szymonpogwizd <szymonpogwizd12@gmail.com>
2023-12-09 14:26:30 +0100

    JZ-84 - Implement CourseController and CourseCategoryController

---
+++ src/main/java/jazdaz/JazdaZ/controller/CourseController.java
@@ -0,0 +1,56 @@
+package jazdaz.JazdaZ.controller;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jazdaz.JazdaZ.database.course.*;
+import jazdaz.JazdaZ.service.CourseService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Log4j2
+@RestController
+@RequestMapping("/courses")
+@RequiredArgsConstructor
+@CrossOrigin
+public class CourseController {
+
+    private final CourseService courseService;
+    private final CourseMapper courseMapper;
+
+    @PostMapping
+    public CourseInfoDTO createCourse(@RequestBody @Valid CourseCreateDTO course) {
+        log.debug("Create course {}", course);
+        CourseEntity toCreate = courseMapper.courseCreateDTO2CourseEntity(course);
+        CourseEntity createdCourse = courseService.create(toCreate);
+        return log.traceExit(courseMapper.courseEntity2CourseInfoDTO(createdCourse));
+    }
+
+    @PutMapping("{id}")
+    public CourseInfoDTO updateCourse(@RequestBody @Valid CourseUpdateDTO course, @PathVariable UUID id) {
+        log.debug("Update course {}: {}", id, course);
+        CourseEntity updatedCourse = courseService.update(id, courseMapper.courseUpdateDTO2CourseEntity(course));
+        return log.traceExit(courseMapper.courseEntity2CourseInfoDTO(updatedCourse));
+    }
+
+    @GetMapping
+    public List<CourseInfoDTO> getAll() {
+        log.debug("Getting all courses");
+        return log.traceExit(
+                courseService.getAll()
+                        .stream()
+                        .map(courseMapper::courseEntity2CourseInfoDTO)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteCourse(@PathVariable UUID id) {
+        log.debug("Deleting course {}", id);
+        courseService.delete(id);
+    }
+}
