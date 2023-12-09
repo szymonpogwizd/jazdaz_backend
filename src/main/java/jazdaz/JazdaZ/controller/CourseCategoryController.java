bdf8d8e5bf7306606a5d117dfc88b8721a6a098a
Szymon Pogwizd <116025180+szymonpogwizd@users.noreply.github.com>
2023-12-09 14:26:57 +0100

    JZ-84 - Implement CourseController and CourseCategoryController (#17)

---
+++ src/main/java/jazdaz/JazdaZ/controller/CourseCategoryController.java
@@ -0,0 +1,55 @@
+package jazdaz.JazdaZ.controller;
+
+import jazdaz.JazdaZ.database.course.courseCategory.*;
+import jazdaz.JazdaZ.service.CourseCategoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Log4j2
+@RestController
+@RequestMapping("/coursesCategories")
+@RequiredArgsConstructor
+@CrossOrigin
+public class CourseCategoryController {
+
+    private final CourseCategoryService courseCategoryService;
+    private final CourseCategoryMapper courseCategoryMapper;
+
+    @PostMapping
+    public CourseCategoryInfoDTO createCourseCategory(@RequestBody @Valid CourseCategoryCreateDTO courseCategory) {
+        log.debug("Create course category {}", courseCategory);
+        CourseCategoryEntity toCreate = courseCategoryMapper.courseCategoryCreateDTO2CourseCategoryEntity(courseCategory);
+        CourseCategoryEntity createdCourseCategory = courseCategoryService.create(toCreate);
+        return log.traceExit(courseCategoryMapper.courseCategoryEntity2CourseCategoryInfoDTO(createdCourseCategory));
+    }
+
+    @PutMapping("{id}")
+    public CourseCategoryInfoDTO updateCourseCategory(@RequestBody @Valid CourseCategoryUpdateDTO courseCategory, @PathVariable UUID id) {
+        log.debug("Update course category {}: {}", id, courseCategory);
+        CourseCategoryEntity updatedCourseCategory = courseCategoryService.update(id, courseCategoryMapper.courseCategoryUpdateDTO2CourseCategoryEntity(courseCategory));
+        return log.traceExit(courseCategoryMapper.courseCategoryEntity2CourseCategoryInfoDTO(updatedCourseCategory));
+    }
+
+    @GetMapping
+    public List<CourseCategoryInfoDTO> getAll() {
+        log.debug("Getting all course categories");
+        return log.traceExit(
+                courseCategoryService.getAll()
+                        .stream()
+                        .map(courseCategoryMapper::courseCategoryEntity2CourseCategoryInfoDTO)
+                        .collect(Collectors.toList())
+        );
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteCourseCategory(@PathVariable UUID id) {
+        log.debug("Deleting course category {}", id);
+        courseCategoryService.delete(id);
+    }
+}
